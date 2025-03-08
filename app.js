dfff470451c9fc80085a08baaa0e519f2b932143
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-03-08 21:24:48 +0300

    changed files

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
