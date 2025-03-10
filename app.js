59440f560022bf4b3f53f0ba4f10434b87a26f69
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-03-10 21:39:26 +0300

    model file is created

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const app = express();
+const Photo = require('./models/Photo');
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/addphoto', (req, res) => {
   res.render('addphoto');
+});
+app.post('/photos', (req, res) => {
+  Photo.create(req.body);
+  res.redirect('/');
 });
 app.get('/about', (req, res) => {
   res.render('about');
